a3afb95e4dc746b29b14c16c0077fc8ce3f085d3
Discolover <36002599+Discolover@users.noreply.github.com>
2019-01-01 13:57:37 +0300

    Add files via upload
    example of swipe handling

---
+++ SwipeHandle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeHandle : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       this.GetComponent<SwipeDetection>().NewSwipe += SwipeHandle_NewSwipe;
+    }
+
+    private void SwipeHandle_NewSwipe(object sender, SwipeEventArgs e)
+    {
+        Debug.Log(e.GetDirection.ToString());
+    }
+}
